01a1d771be7930ed2060766eb9af081756ccc8c5
andrey <supev7@gmail.com>
2025-09-25 11:13:00 +0200

    starting implementing

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,18 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
-export class AppComponent {
-  title = 'my-app';
-}
-
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { MoviesPageComponent } from './pages/movies-page/movies-page.component';
