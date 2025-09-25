7d83b6d04ebec1723719262c6e4be92cb6ea6ef5
andrey <supev7@gmail.com>
2025-09-25 16:13:58 +0200

    impementing people list

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { DetailViewComponent } from "./components/detail-view/detail.component";
 import { MoviesPageComponent } from "./pages/movies-page/movies-page.component";
+import { PeoplePageComponent } from "./pages/people-page/people-page.component";
 
 export const routes: Routes = [
   { path: 'films-list', component: MoviesPageComponent },
   { path: 'films-detail', component: DetailViewComponent },
+  { path: 'people-list', component: PeoplePageComponent },
   { path: '', redirectTo: 'films-list', pathMatch: 'full' },
   { path: '**', redirectTo: 'films-list' }
 ];
--- my-app/src/app/pages/movies-page/movies-page.component.ts
+++ my-app/src/app/pages/movies-page/movies-page.component.ts
@@ -9,8 +9,36 @@ import { MovieListComponent } from '../../components/movie-list/movie-list.compo
   selector: 'app-movies-page',
   standalone: true,
   imports: [CommonModule, MovieListComponent],
-  templateUrl: './movies-page.component.html',
-  styleUrls: ['./movies-page.component.scss']
+  template: `
+    <div class="container movies-page-container">
+      <h1 class="page-title">Filme</h1>
+      <app-movie-list [movies]="movies$ | async"></app-movie-list>
+    </div>
+  `,
+  styles: [`
+    .movies-page-container {
+      padding-top: 64px;
+      padding-bottom: 64px;
+    }
+    .page-title {
+      font-size: 3.5rem;
+      font-weight: 700;
+      color: #0A1931;
+      text-align: center;
+      margin-top: 0;
+      margin-bottom: 64px;
+    }
+    @media (max-width: 768px) {
+      .page-title {
+        font-size: 2.5rem;
+        margin-bottom: 48px;
+      }
+      .movies-page-container {
+        padding-top: 48px;
+        padding-bottom: 48px;
+      }
+    }
+  `]
 })
 export class MoviesPageComponent implements OnInit {
   movies$!: Observable<Movie[]>;
