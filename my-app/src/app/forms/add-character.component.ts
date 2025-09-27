3bbe0cc131e9e524b3f07a0018b0058233a9687f
andrey <supev7@gmail.com>
2025-09-27 11:22:15 +0200

    stylings

--- my-app/src/app/forms/add-character.component.ts
+++ my-app/src/app/forms/add-character.component.ts
@@ -72,10 +72,8 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   `,
   styles: [
     `
-      /* Load Inter font only for this component */
       @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap');
 
-      /* Scoped reset and base styles for AddCharacter component */
       :host, :host * {
         box-sizing: border-box;
       }
